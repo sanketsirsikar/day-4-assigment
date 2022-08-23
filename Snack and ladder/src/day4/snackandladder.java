ee3bd3a29ac3877ab42cf42603569f1a47b42e52
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-08-23 19:55:33 +0530

    single player at start position 0

---
+++ Snack and ladder/src/day4/snackandladder.java
@@ -0,0 +1,10 @@
+package day4;
+
+public class snackandladder {
+	public static void main(String[] args) {
+		
+		int startpostion  = 0;
+		System.out.println("start to position"+    startpostion  );
+		
+}
+}
