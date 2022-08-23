c94b9076a6fc772ebf015ad70f00e05f110b2b16
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-08-23 23:27:27 +0530

    Repeat till the Playerreaches the winning position 100.

--- Snack and ladder/src/day4/snackandladder.java
+++ Snack and ladder/src/day4/snackandladder.java
@@ -3,26 +3,35 @@ package day4;
 public class snackandladder {
 	public static void main(String[] args) {
 		
-		int position =0;
-		int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
-		System.out.println("dice number is " + diceNumber);
-		
-		int options = (int) Math.floor(Math.random() *10 ) % 3 ;
-		System.out.println("player option is"  + options);
-		 
-		switch (options) {
-		case 1 : 
-			position  = diceNumber + position;
-			System.out.println("ladder");
-			System.out.println("position of the playar after the ladder" + position);
-		
-		case 2 :
-			position = position - diceNumber;
-			System.out.println("snack");
-			System.out.println("position of the player after the snack" + position);
-		default :
-			System.out.println("no play");
+		int position = 0;
+		  while (position < 100) {
+			  int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+	            System.out.println("diceNumber is : " + diceNumber);
+	            
+	            int option = (int) Math.floor(Math.random() * 10) % 3;
+	            System.out.println("selected player option : " + option);
+	            
+	            switch (option) {
+                case 1:
+                    position = diceNumber + position;
+                    System.out.println("Ladder");
+                    System.out.println("position for the player after the ladder is : " + position);
+                    break;
+                    
+                case 2:
+                    position = position - diceNumber;
+                    System.out.println("Snack");
+                    if (position < 0) {
+                        position = 0;
+                        System.out.println("Player restart from zero: ");
+                    }
+                    System.out.println("position for the player after the Snake is : " + position);
+                    break;
+	            
+                default:
+                    System.out.println("No play");
 			}
 		
+}
 }
 }
