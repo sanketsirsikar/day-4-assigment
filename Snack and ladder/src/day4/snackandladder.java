a93ccd2c7470449653d444b3dd71d3f5f7722ab6
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-08-23 23:33:38 +0530

    Ensure the player getsto exact winningposition 100

--- Snack and ladder/src/day4/snackandladder.java
+++ Snack and ladder/src/day4/snackandladder.java
@@ -3,33 +3,37 @@ package day4;
 public class snackandladder {
 	public static void main(String[] args) {
 		
+		int WINNING_POSITION = 100;
 		int position = 0;
-		  while (position < 100) {
-			  int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
-	            System.out.println("diceNumber is : " + diceNumber);
+		while (position < WINNING_POSITION) {
+			 int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+	            System.out.println("DiceNumber is : " + diceNumber);
+	            int option = (int) Math.floor(Math.random() * 10) % 2;
+	            System.out.println("Selected player option : " + option);
 	            
-	            int option = (int) Math.floor(Math.random() * 10) % 3;
-	            System.out.println("selected player option : " + option);
-	            
+
 	            switch (option) {
-                case 1:
-                    position = diceNumber + position;
-                    System.out.println("Ladder");
-                    System.out.println("position for the player after the ladder is : " + position);
-                    break;
-                    
-                case 2:
-                    position = position - diceNumber;
-                    System.out.println("Snack");
-                    if (position < 0) {
-                        position = 0;
-                        System.out.println("Player restart from zero: ");
-                    }
-                    System.out.println("position for the player after the Snake is : " + position);
-                    break;
-	            
-                default:
-                    System.out.println("No play");
+	                case 1:
+	                    position = diceNumber + position;
+	                    System.out.println("Ladder");
+	                    if (position > 100) {
+	                        position = 100;
+	                        position = position - diceNumber;
+	                    }
+	                    System.out.println("Position for the player after the ladder is : " + position);
+	                    break;
+	                case 2:
+	                    position = position - diceNumber;
+	                    System.out.println("Snack");
+	                    if (position < 0) {
+	                        position = 0;
+	                        System.out.println("Player restart from zero: ");
+	                    }
+	                    System.out.println("Position for the player after the Snake is : " + position);
+	                    break;
+	                default:
+	                    System.out.println("No play");
+
 			}
 		
 }
